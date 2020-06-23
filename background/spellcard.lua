7a268fc743a8df6ca32d103451430b99dc32a631
Xiliusha <Xiliusha@outlook.com>
2020-06-23 16:01:45 +0800

    还是背景

--- background/spellcard.lua
+++ background/spellcard.lua
@@ -59,12 +59,3 @@ function _spellcard_background:render()
         end
     end
 end
-
-spellcard_background = Class(_spellcard_background)
-function spellcard_background:init()
-    LoadImageFromFile("_scbg", "THlib/background/spellcard/background.png", true, 0, 0, false)
-    LoadImageFromFile("_scbg_mask", "THlib/background/spellcard/mask.png", true, 0, 0, false)
-    _spellcard_background.init(self)
-    _spellcard_background.AddLayer(self, "_scbg_mask", true, 0, 0, 0, 1, 1, 0, "", 1, 1, nil, nil)
-    _spellcard_background.AddLayer(self, "_scbg", false, 0, 0, 0, 0, 0, 0, "", 1, 1, nil, nil)
-end
